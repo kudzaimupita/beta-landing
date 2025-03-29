4b99d5fabb2ff4052d20e1927cd1f751f420269f
Eddie <21100419@virtualwindow.co.za>
2025-03-29 03:41:52 +0200

    redirect broken link

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,31 +1,31 @@
 import { FC, useEffect } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import { Layout } from "@/app/Layout";
-import { Home, NoMatch } from "@/pages";
+import { Home } from "@/pages";
 import ComingSoonPage from "@/pages/ComingSoon";
 import Texture from '../assets/img/Texture.png';
 import { ConsoleText } from "@/components/consoleText";
 
 const App: FC = () => {
-
   useEffect(() => {
     console.log(`
-    
-  We're hiring soon! Visit https://servly.app/careers
-  
-  ${ConsoleText}
-
-  `);
+      We're hiring soon! Visit https://servly.app/careers
+      ${ConsoleText}
+    `);
   }, []);
 
   return (
     <>
-      <img className="absolute opacity-[0.5] z-[9999] h-[100vh] w-[100vw] mix-blend-multiply pointer-events-none"
-        src={Texture} alt="Texture" />
+      <img
+        className="absolute opacity-[0.5] z-[9999] h-[100vh] w-[100vw] mix-blend-multiply pointer-events-none"
+        src={Texture}
+        alt="Texture"
+      />
       <Routes>
         {/* <Route path="/" element={<Layout />}> */}
         <Route index element={<ComingSoonPage />} />
-        <Route path="*" element={<NoMatch />} />
+        {/* Catch-all route - redirects all 404s to index */}
+        <Route path="*" element={<Navigate to="/" replace />} />
         {/* </Route> */}
       </Routes>
     </>
